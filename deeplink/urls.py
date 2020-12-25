711d0717e4f7cecbc526ac460bf2610740b7662a
james.zhang <2665353662@qq.com>
2020-12-25 18:33:03 +0800

    1. register deeplink model to admin.

--- deeplink/urls.py
+++ deeplink/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path
+from django.urls import path, re_path
+from django.views.generic.base import RedirectView
 
 from deeplink import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    re_path(r'^favicon\.ico$', RedirectView.as_view(url=r'static/favicon.ico')),
     path('list/<slug:project>/', views.deeplink_list, name='deeplink_list'),
     path('edit/<slug:project>/', views.add_deeplink, name='deeplink_edit'),
     path('removeProject/', views.remove_project, name='remove_project'),
