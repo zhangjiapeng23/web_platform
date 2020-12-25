ee2bbd857171e95f588a537ed75df0e6bacb291f
james.zhang <2665353662@qq.com>
2020-12-25 18:33:29 +0800

    1. register qa_tools models to admin.

--- qa_tools/admin.py
+++ qa_tools/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from .models import Project, Notification
 # Register your models here.
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ['nid', 'name', 'api_key']
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ['nid', 'project', 'get_type_display']
+
+
+
