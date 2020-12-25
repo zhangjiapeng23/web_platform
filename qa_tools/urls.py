77c231805cc9e87167dd5e9d856130af2560f061
james.zhang <2665353662@qq.com>
2020-12-25 18:34:22 +0800

    1. Update qa_tools url path.

--- qa_tools/urls.py
+++ qa_tools/urls.py
@@ -5,5 +5,7 @@ from qa_tools import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    
+    path('brazeNotification/', views.braze_notification, name='braze_notification'),
+    path('brazeNotification/<slug:project>', views.notification_detail, name='notification_detail'),
+
 ]
