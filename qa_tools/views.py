123226905bfd606ac44c1616184ba988a7ac1404
张佳鹏 <18373230129@163.com>
2020-12-27 23:53:57 +0800

    1. update qa_tools notification detail add item function.

--- qa_tools/views.py
+++ qa_tools/views.py
@@ -1,6 +1,8 @@
+import collections
 
 from django.shortcuts import render, HttpResponse
 from django.http import JsonResponse
+from django.db.models import Count
 
 from qa_tools import models
 # Create your views here.
@@ -45,10 +47,9 @@ def braze_notification(request):
 
 def notification_detail(request, project):
     response = dict()
-    deeplink_type = models.Notification.objects.filter(project__name=project, type=1).all()
-    push_type = models.Notification.objects.filter(project__name=project, type=0).all()
-    general_type = models.Notification.objects.filter(project__name=project, type=2).all()
-    print(deeplink_type)
+    deeplink_type = models.Notification.objects.filter(project__name=project, push_type=1).all()
+    push_type = models.Notification.objects.filter(project__name=project, push_type=0).all()
+    general_type = models.Notification.objects.filter(project__name=project, push_type=2).all()
     response['deeplink'] = deeplink_type
     response['push_type'] = push_type
     response['general'] = general_type
@@ -56,3 +57,60 @@ def notification_detail(request, project):
     return render(request, 'qa_tools/notificationDetail.html', context={"project_name": project,
                                                                         "response": response,
                                                                         })
+
+
+def send_braze_push(request):
+    if request.is_ajax():
+        response = {'code': 'fail', 'msg': None}
+        push_dict = collections.defaultdict(list)
+        project_info = dict()
+        project_name = request.POST.get('project')
+        selected_items = request.POST.getlist('selected_items[]')
+        test_account = request.POST.get('test_account')
+        if not test_account:
+            response['msg'] = 'Test account is required.'
+        else:
+            notifications = models.Notification.objects.filter(pk__in=selected_items).all()
+            for notification in notifications:
+                push_dict[notification.get_push_type_display()].append(notification.content)
+            projcet_obj = models.Project.objects.filter(name=project_name).first()
+            project_info['instance_url'] = projcet_obj.instance_url
+            project_info['name'] = project_name
+            project_info['scheme'] = projcet_obj.scheme
+            braze_notification_console(project_info, push_dict)
+
+        return JsonResponse(response)
+
+
+def braze_notification_console(project: dict, push: dict):
+
+
+
+    print(project, push)
+
+
+def add_push(request):
+    if request.is_ajax():
+        response = {'code': 'fail', 'msg': None, 'id': None}
+        push_type_dict = {
+            "deeplink": 1,
+            "general": 2,
+            "push_type": 0,
+        }
+        project_name = request.POST.get('project_name')
+        push_type = request.POST.get('push_type')
+        push_content = request.POST.get('push_content')
+        if not push_content:
+            response['msg'] = "It's required."
+        else:
+            project_obj = models.Project.objects.filter(name=project_name).first()
+            res = models.Notification.objects.create(push_type=push_type_dict[push_type], content=push_content, project=project_obj)
+            if res:
+                response['code'] = 'success'
+                response['msg'] = '%s add successful.' % res
+                response['id'] = res.nid
+            else:
+                response['msg'] = 'Add push failed, please try again.'
+
+        return JsonResponse(response)
+            
