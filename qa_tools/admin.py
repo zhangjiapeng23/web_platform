08a73527cda9af2c03d82f7c6fbf0f3dc166b679
张佳鹏 <18373230129@163.com>
2021-02-16 21:04:51 +0800

    1. add sdk config model to admin.

--- qa_tools/admin.py
+++ qa_tools/admin.py
@@ -1,15 +1,22 @@
 from django.contrib import admin
 
-from .models import Project, Notification
+from .models import Project, Notification, SdkConifg
 # Register your models here.
+
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ['nid', 'name', 'api_key']
 
+
 @admin.register(Notification)
 class NotificationAdmin(admin.ModelAdmin):
     list_display = ['nid', 'project', 'get_push_type_display', 'content']
+
+
+@admin.register(SdkConifg)
+class SdkConfigAdmin(admin.ModelAdmin):
+    list_display = ['nid', 'project_name', 'config_type', 'app_key']
 
 
 
