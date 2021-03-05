566467fc456b385bbe317a08d3ed0a30946b45bd
james.zhang <2665353662@qq.com>
2021-03-05 18:36:08 +0800

    1. add project info android and iOS uploade api unittest.

--- project_info/tests.py
+++ project_info/tests.py
@@ -1,3 +1,31 @@
+import os
+
+import requests
 from django.test import TestCase
 
 # Create your tests here.
+
+
+def test_android_upload_api():
+    android_build = os.path.join(os.path.dirname(__file__), 'testdata', 'Android_build_info.json')
+    with open(android_build, 'r', encoding='utf-8') as fp:
+        data = fp.read()
+
+    # print(type(data))
+    headers = {'Content-Type': 'application/json'}
+    res = requests.post("http://127.0.0.1:50001/projectInfo/NLAndroid/",
+                        headers=headers, data=data)
+    print(res.status_code)
+
+def test_ios_upload_api():
+    ios_build = os.path.join(os.path.dirname(__file__), 'testdata', 'iOS_build_info.json')
+    with open(ios_build, 'r', encoding='utf-8') as fp:
+        data = fp.read()
+
+    headers = {'Content-Type': 'application/json'}
+    res = requests.post("http://127.0.0.1:50001/projectInfo/NLiOS/",
+                        headers=headers, data=data)
+    print(res.status_code)
+
+
+
