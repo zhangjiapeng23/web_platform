fbfefeffea7bbd8df2eb355e3494161145ba06b8
james.zhang <2665353662@qq.com>
2021-03-09 10:01:29 +0800

    1. create project info templatetags files.

--- project_info/templatetags/project_info_filter.py
+++ project_info/templatetags/project_info_filter.py
@@ -2,3 +2,34 @@
 # -*- encoding: utf-8 -*-
 # @author: James Zhang
 # @data  : 2021/3/8
+import re
+
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def library_name_format(library_name: str):
+    return library_name.split(':')[1]
+
+
+@register.filter
+def ios_x_framework_check(framework_version: str):
+    if re.match(r'\d+\.\d+\.0\d+', framework_version) \
+            or 'x' in str(framework_version):
+        return True
+    else:
+        return False
+
+
+@register.filter
+def is_library_need_upgrade(library_name, library_version):
+    needupdate = ['5.0.0.x', '5.0.0', '5.0.02', '5.0.03', '5.0.04', '5.0.1', '5.0.1', '5.0.2', '5.0.3']
+    if str(library_name) == 'NLTracking' and (int(str(library_version).replace('.', '')) < 448 or
+                                             str(library_version) in needupdate):
+        return True
+    else:
+        return False
+
+
