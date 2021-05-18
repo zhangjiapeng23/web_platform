c5348c21896a051c9df912409a6a810ad61ff19e
jameszhang <18373230129@163.com>
2021-05-18 18:34:50 +0800

    1. add '/' to path end.

--- project_info/urls.py
+++ project_info/urls.py
@@ -10,8 +10,8 @@ urlpatterns = [
     path("", views.index, name='index'),
     path("AndroidProjectList/", views.android_project_list, name='android_project_list'),
     path("iOSProjectList/", views.ios_project_list, name='ios_project_list'),
-    path("AndroidProjectList/<str:project>", views.android_project_detail, name='android_project_detail'),
-    path("iOSProjectList/<str:project>", views.ios_project_detail, name='ios_project_detail'),
+    path("AndroidProjectList/<str:project>/", views.android_project_detail, name='android_project_detail'),
+    path("iOSProjectList/<str:project>/", views.ios_project_detail, name='ios_project_detail'),
     path("AndroidLibraryDetail/", views.android_library_detail, name='android_library_detail'),
     path("iOSLibraryDetail/", views.ios_library_detail, name='ios_library_detail'),
 
